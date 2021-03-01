212b51d10f33e304309a36bd243da389da4b7d66
Juan Pablo Varela <jvarelaaloisio@users.noreply.github.com>
2021-03-01 15:14:21 -0300

    New action and bugfixes
    - BugFixes:
    ActionWithFrequency didn't subscribe to the manager
    Changes to naming conventions
    - New Features:
    Created ActionWithPeriod.cs

--- Runtime/ActionWithFrequency.cs
+++ Runtime/ActionWithFrequency.cs
@@ -1,51 +1,9 @@
 ﻿using System;
-using UnityEngine;
 
 namespace Packages.UpdateManagement
 {
-	public class ActionWithFrequency : IUpdateable
+	class ActionWithFrequency : ActionWithPeriod
 	{
-		private float _currentTime = 0;
-		public float Period { get; }
-		public bool IsRunning { get; private set; }
-		private readonly Action _action;
-
-		public ActionWithFrequency(Action action, float period)
-		{
-			Period = period;
-			_action = action;
-		}
-
-		/// <summary>
-		/// Subscribes to updateManager and starts ticking
-		/// </summary>
-		public void StartAction()
-		{
-			IsRunning = true;
-			_currentTime = 0;
-			if (!IsRunning)
-				UpdateManager.Subscribe(this);
-		}
-
-		/// <summary>
-		/// Stops ticking and unsubscribes from updateManager
-		/// </summary>
-		public void StopAction()
-		{
-			IsRunning = false;
-			UpdateManager.UnSubscribe(this);
-		}
-
-		/// <summary>
-		/// Method used for UpdateManager. Don't Call.
-		/// </summary>
-		public void OnUpdate()
-		{
-			_currentTime += Time.deltaTime;
-			if (_currentTime < Period)
-				return;
-			_currentTime = 0;
-			_action();
-		}
+		public ActionWithFrequency(Action action, float frequency) : base(action, 1/frequency) { }
 	}
 }
---
+++ Runtime/ActionWithPeriod.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace Packages.UpdateManagement
+{
+	public class ActionWithPeriod : IUpdateable
+	{
+		private float _currentTime = 0;
+		public float Period { get; }
+		public bool IsRunning { get; private set; }
+		private readonly Action _action;
+
+		public ActionWithPeriod(Action action, float period)
+		{
+			Period = period;
+			_action = action;
+		}
+
+		/// <summary>
+		/// Subscribes to updateManager and starts ticking
+		/// </summary>
+		public void StartAction()
+		{
+			_currentTime = 0;
+			if (!IsRunning)
+				UpdateManager.Subscribe(this);
+			IsRunning = true;
+		}
+
+		/// <summary>
+		/// Stops ticking and unsubscribes from updateManager
+		/// </summary>
+		public void StopAction()
+		{
+			IsRunning = false;
+			UpdateManager.UnSubscribe(this);
+		}
+
+		/// <summary>
+		/// Method used for UpdateManager. Don't Call.
+		/// </summary>
+		public void OnUpdate()
+		{
+			_currentTime += Time.deltaTime;
+			if (_currentTime < Period)
+				return;
+			_currentTime = 0;
+			_action();
+		}
+	}
+}
